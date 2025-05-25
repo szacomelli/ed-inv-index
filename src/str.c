dfdd24ac37b6082692b700f947c168c89c93be65
JDomingueti <jean.domingueti@gmail.com>
2025-05-25 18:09:36 -0300

    Adding some str functions

--- src/str.c
+++ src/str.c
@@ -1,5 +1,4 @@
 #include "str.h"
-#include <stdlib.h>
 
 int strcmp(string wordOne, string wordTwo) {
     int result = 0;
@@ -19,19 +18,82 @@ int strcmp(string wordOne, string wordTwo) {
     return result;
 }
 
-int strsize(string word) {
-    return (sizeof(word)/sizeof(char));
+int countChar(string txt, char wanted) {
+    if (!txt) return 0;
+    int count = 0;
+    int idx = 0;
+    while (txt[idx]) if(txt[idx++] == wanted) count++;
+    return count;
 }
 
-void strcopy(string src, string dst, uint start, uint end) {
-    if (capacity(dst) < capacity(src)) {
-        printf("Error in strcopy(): src bigger than dst;\n");
+int strSize(string word) {
+    if (!word) {
+        printf("Error in strsize(): 'word' param is NULL");
         exit(1);
     }
-    uint idx = 0;
-    while (idx + start < end) {
-        dst[idx + start] = src[idx];
+    uint len = 0;
+    while(word[len]) len++;
+    return len;
+}
+
+void strImplant(string src, string dst, uint start, uint end) {
+    if (!src) {
+        printf("Error in strImplant(): 'src' pointer is NUL;\n");
+        exit(1);
+    }
+    uint len = strSize(dst);
+    uint idx = start;
+    while (idx < end) {
+        dst[idx] = src[idx - start];
         idx++;
     }
-    dst[idx + start] = '\0';
+    if ((idx + start) > len)
+        dst[idx + start] = '\0';
+}
+
+void strCopy(string src, string dst) {
+    if (!src) {
+        printf("Error in strcopy(): 'src' pointer is NULL;\n");
+        exit(1);
+    }
+    uint len = strSize(src);
+    uint idx = 0;
+    while (idx < len) dst[idx++] = src[idx];
+    dst[idx] = '\0';
+}
+
+void strConcat(string str1, string str2, string result, char sep) {
+    if (!str1 | !str2) {
+        printf("Error in strConcat(): 'str' pointer is NULL;\n");
+        exit(1);
+    }
+    uint idxR = 0;
+    uint idx = 0;
+    while(str1[idx]) result[idxR++] = str1[idx++];
+    if (sep) result[idxR++] = sep;
+    idx = 0;
+    while(str2[idx]) result[idxR++] = str2[idx++];
+    result[idxR] = '\0';
+}
+
+string* strSplit(string str, char sep) {
+    if (!str) {
+        return NULL;
+    }
+    uint idx = 0;
+    uint qtd = 1 + countChar(str, sep);
+    idx = 0;
+    string* result = malloc(sizeof(string)*(qtd+1));
+    qtd = 1;
+    result[0] = str;
+    while (str[idx]) { 
+        if ((str[idx] == sep) && str[idx+1]) {
+            str[idx] = '\0';
+            result[qtd] = (str + idx + 1);
+            qtd++;
+        }
+        idx++;
+    }
+    result[qtd] = NULL;
+    return result;
 }
--- src/str.h
+++ src/str.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define string char*
 #define uint unsigned int
@@ -6,7 +7,15 @@
 
 int strcmp(string wordOne, string wordTwo);
 
-int strsize(string word);
+/* Count the occurrences of the char `wanted` in the string
+*  `txt`
+*/
+int countChar(string txt, char wanted);
+
+/* Returns the lenght of the string occupied. It stops
+*  at the first `\0` character
+*/
+int strSize(string word);
 
 /* Example :
 *  > char dst[15] = "abcdefg";
@@ -15,4 +24,21 @@ int strsize(string word);
 *  > printf("%s", dst);
 *  >> abcMakeCopy
 */
-void strcopy(string src, string dst, uint start, uint end);
+void strImplant(string src, string dst, uint start, uint end);
+
+/* Copy the content of string `src` in string `dst`
+*/
+void strCopy(string src, string dst);
+
+/* Concatenates the strings `str1` and `str2` in the string
+*  `result`. If the `sep` is set to `\0`, there won't have
+*  a separator character 
+*/
+void strConcat(string str1, string str2, string result, char sep);
+
+/* Returns a list of pointers to the splitted content. The 
+*  content of `str` is modified so if you are going to use
+*  the string yet, make a copy before use this function.
+*  The returned pointer must be freed.
+*/
+string* strSplit(string str, char sep);
