1dfa95b09152f14d2fb024022378f484fda1d22e
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2025-05-29 17:30:56 -0300

    early versions of printIndex and saveTree implemented

--- src/tree_utils.c
+++ src/tree_utils.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "tree_utils.h"
 
-tNode* createNode() {
+tNode* createtNode() {
     tNode* node = malloc(sizeof(tNode));
     node->word = NULL;
 
@@ -21,4 +21,81 @@ bTree* createTree() {
     tree->root = NULL;
     tree->NIL = NULL;
     return tree;
+}
+
+int prtIndexAux(tNode* tnode, int idx) {
+    if (tnode == NULL) return idx - 1;
+    printf("%d. %s: ", idx, tnode->word);
+    node* iterator = tnode->documentIds->head;
+
+    while (iterator != NULL) {
+        if (iterator->index > 0) printf(", ");
+        printf("%d", *(int*)iterator->value);
+
+        iterator = iterator->next;
+    }
+    printf(".\n");
+    int currIndex = prtIndexAux(tnode->left, idx + 1);
+    currIndex = prtIndexAux(tnode->right, currIndex + 1);
+    return currIndex;
+
+}
+
+void printIndex(bTree* tree) {
+    if (tree == NULL) return;
+    tNode* iterator = tree->root;
+    int currIndex = prtIndexAux(iterator, 0);
+    return;
+}
+
+int getMaxID(tNode* node) {
+    if (node == NULL) return 0;
+
+    int left = getMaxID(node->left);
+    int right = getMaxID(node->right);
+
+    int currID = node->documentIds->size;
+    if (right > currID) currID = right;
+    if (left > currID) currID = left;
+
+    return currID;
+}
+
+void svTreeAux(tNode* node, int maxID, FILE* file) {
+    if (node == NULL) return;
+    int color1 = 200*(((float) node->documentIds->size)/maxID) + 55;
+
+    if (((float) node->documentIds->size)/maxID < 0.5)
+
+    fprintf(file, "\t%s [fillcolor = \"#%02x%02x%02x\", fontcolor = \"%s\", color = \"%s\"]\n\t%s",
+        node->word, color1,0,0,(((float) node->documentIds->size)/maxID < 0.5 ? "white" : "black"),
+        ((float) node->documentIds->size)/maxID < 0.5 ? "white" : "black",node->word);
+    if (node->right && node->left) {
+        fprintf(file, "-> {%s %s}\n", node->right->word, node->left->word);
+        svTreeAux(node->left, maxID, file);
+        svTreeAux(node->right, maxID, file);
+    }
+    else if (node->left && !node->right || !node->left && node->right ) {
+        fprintf(file, "-> %s\n", node->right ? node->right : node->left);
+        svTreeAux(node->right ? node->right : node->left, maxID, file);
+    }
+    else fprintf(file, "\n");
+
+    return;
+}
+
+void saveTree(bTree* tree) {
+    FILE* file = fopen("test.txt", "w");
+    if (file == NULL) return;
+
+    fprintf(file, "digraph {\n\tbgcolor=\"bisque\"\n\tnode [style=\"filled\", shape=\"component\"]\n");
+
+    int maxID = getMaxID(tree->root);
+    svTreeAux(tree->root, maxID, file);
+
+    fprintf(file, "}\n");
+    fclose(file);
+
+    system("mkdir graph ; dot -Tsvg ./test.txt > ./graph/output.svg");
+    return;
 }
